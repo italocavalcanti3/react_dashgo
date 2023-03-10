8b4818bd3d15a2d669db613b7cfed9c31488b51b
Italo Cavalcanti <italobc93@gmail.com>
2023-03-10 11:57:04 -0300

    responsivo e sidebar ajustados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Flex } from '@chakra-ui/react';
+import { useSidebarDrawer } from '@/contexts/SidebarDrawerContext';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Flex
       as="header"
@@ -13,15 +22,28 @@ export function Header() {
       h="20"
       mx="auto"
       mt="4"
-      align="center"
       px="6"
+      align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label='Open navigation'
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+          display="flex"
+          alignItems="center"
+        />
+      )}
+
       <Logo />
-      <SearchBox />
+      { isWideVersion && <SearchBox /> }
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
